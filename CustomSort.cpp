4ad491abb77f2b423c8f842f41d7ef9642c7994b
nikita1503 <agarwal1503nikita@gmail.com>
2022-07-17 18:09:48 +0530

    Update CustomSort.cpp

--- CustomSort.cpp
+++ CustomSort.cpp
@@ -1,4 +1,6 @@
- Sort 'arr' based on the score of each element. Score is store in array 'score'
+Sort arr in descending order
+sort(arr.begin(), arr.end(), greater <>())
+Sort 'arr' based on the score of each element. Score is store in array 'score'
  sort(arr.begin(), arr.end(), [&](int x, int y)
   {
     return score[x] > score[y];
