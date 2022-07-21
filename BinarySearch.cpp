39840606706cb6999d0609aa9bd93c96d1990432
nikita1503 <agarwal1503nikita@gmail.com>
2022-07-22 02:52:21 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,1 +1,12 @@
-
+//low and high are inclusive
+binarySearch(arr, x, low, high)
+    if low > high
+        return False 
+    else
+        mid = (low + high) / 2 
+        if x == arr[mid]
+            return mid
+        else if x > arr[mid]        // x is on the right side
+            return binarySearch(arr, x, mid + 1, high)
+        else                               // x is on the left side
+            return binarySearch(arr, x, low, mid - 1)
