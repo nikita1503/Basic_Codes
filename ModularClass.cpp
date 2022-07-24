72f4308d0066d3690b53d306ad8abf38c0bc73db
nikita1503 <agarwal1503nikita@gmail.com>
2022-07-24 05:30:17 +0530

    Update ModularClass.cpp

--- ModularClass.cpp
+++ ModularClass.cpp
@@ -1,21 +1,18 @@
 typedef long long ll;
 struct LL {
   ll value;
-  static const ll MOD = 10e9+7;
+  static const ll MOD = 1000000007;
 
   LL(long long v = 0) { value = v % MOD; if (value < 0) value += MOD;}
   LL(long long a, long long b) : value(0){ *this += a; *this /= b;}
 
   LL& operator+=(LL const& b) {value += b.value; if (value >= MOD) value -= MOD; return *this;}
   LL& operator-=(LL const& b) {value -= b.value; if (value < 0) value += MOD;return *this;}
-  LL& operator*=(LL const& b) {value = (ll)value * b.value % MOD;return *this;}
+  LL& operator*=(LL const& b) {value = ((ll)value * b.value) % MOD;return *this;}
   LL& operator++(int) {LL tmp(*this); value += (ll)1; if (value >= MOD) value -= MOD; return tmp;}
   LL& operator--(int) {LL tmp(*this); value -= (ll)1; if (value < 0) value += MOD;return tmp;}
 
-  friend LL pow_(LL a, ll e) {
-    LL res = 1; while (e) { if (e&1) res *= a; a *= a; e >>= 1; }
-    return res;
-  }
+  friend LL pow_(LL a, ll e) { ll res = 1; while (e > 0) {if (e & 1) res = (res * a.value)%MOD; a = a * a;e >>= 1;} return LL(res); }
   friend LL inverse(LL a) { return pow_(a, MOD - 2); }
 
   LL& operator/=(LL const& b) { return *this *= inverse(b); }
