1f1613b6906a236d00a3ddcb02aeb89a3d5a15c9
nikita1503 <agarwal1503nikita@gmail.com>
2022-07-25 03:46:17 +0530

    Update OrderStatisticTree.cpp

--- OrderStatisticTree.cpp
+++ OrderStatisticTree.cpp
@@ -1,8 +1,11 @@
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
+using namespace std;
 
-tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> X;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ordered_set<pair<int,string>>  X; // this can be any type
 
 //insert in tree - Complexity O(logn)
 X.insert(1);
