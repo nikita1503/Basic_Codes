704b2b4bbb5c29126e010f722dec626b2dbe48fd
nikita1503 <agarwal1503nikita@gmail.com>
2023-05-08 01:18:56 +0530

    Create Floyd-Warshall.cpp

---
+++ Floyd-Warshall.cpp
@@ -0,0 +1,9 @@
+//n=number of nodes
+//initially dist[i][j] = edge length from i->j, if no edge then dist[i][j] = INF
+for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
+        }
+    }
+}
