1e7b7da12be700b2a2079f364922b869df7ec5cc
nikita1503 <agarwal1503nikita@gmail.com>
2024-01-19 18:52:54 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -8,6 +8,9 @@ typedef long long ll;
 typedef long double ld;
 typedef vector<int> VI;
 typedef pair<int,int> PII;
+typedef vector<ll> VL;
+typedef pair<ll,ll> PLL;
 const ll mod=1000000007;
 ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
 //Your magic 😉
+
