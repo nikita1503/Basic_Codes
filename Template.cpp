0ef80b6ba958249be16d833a4f1c559962f19b5b
nikita1503 <agarwal1503nikita@gmail.com>
2024-01-19 18:02:34 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i=0;i<n;i++)
+#define FOR(i,a,b) for (int i=a;i<=b;i++)
+#define pb push_back
+#define mp make_pair
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+const ll mod=1000000007;
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+//Your magic 😉
