b99a8b9939f808c0524db10f46aeb07c3590d0a9
nikita1503 <agarwal1503nikita@gmail.com>
2024-01-19 22:00:56 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -11,6 +11,7 @@ typedef pair<int,int> PII;
 typedef vector<ll> VL;
 typedef pair<ll,ll> PLL;
 const ll mod=1000000007;
+const ll INF=1000000007;
 ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
 void printV(VI&v) {REP(i,v.size())cout<<v[i]<<' ';cout<<'\n';};
 void printV(VL&v) {REP(i,v.size())cout<<v[i]<<' ';cout<<'\n';};
