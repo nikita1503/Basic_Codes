940ca111bfd45b7bd2d32600ef1054a1a2463c94
Nikita Agarwal <agarwal1503nikita@gmail.com>
2025-06-17 16:07:24 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -9,3 +9,21 @@ binarySearch(arr,x)
     //R will point to the just element>=x
     //L will point to the just element<x
 }
+
+//Generalised
+//the array has consecutive 0s followed by consecutive 1s.
+//l is always in 0s, r is always in 1s
+//we will stop when l and r have reached the border, i.e. last 0 and first 1
+//return l is we want last 0, return 1 if we want first 1
+
+binarySearch(min_possible_ans, max_possible_ans)
+    int L = min_possible_ans-1, R = max_possible_ans+1, M;
+    while (L + 1 < R) {
+       M = (L+R)/2;
+    if (predicate(M)==0) L = M; else R = M;
+
+    //if we want last 0
+    return L;
+    // if we want first 1
+    return R;
+}
